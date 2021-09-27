dd614a413a75824e99b664b7ac7b3b7d6b17c92f
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-09-28 00:40:05 +0100

    update

---
+++ core/src/com/isoterik/pongoal/scenes/GameScene.java
@@ -0,0 +1,6 @@
+package com.isoterik.pongoal.scenes;
+
+import com.isoterik.racken.Scene;
+
+public class GameScene extends Scene {
+}
--- desktop/src/com/isoterik/pongoal/desktop/DesktopLauncher.java
+++ desktop/src/com/isoterik/pongoal/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.isoterik.pongoal.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.isoterik.pongoal.Pongoal;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Pongoal(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Pongoal(), config);
 	}
 }
